826f4a673ba82c265497ce966ee09c8cd5c5f864
Emily Ramey <eramey16@lbl.gov>
2023-04-14 11:27:51 -0700

    Messed with the stash and regretted it

--- make_runit.py
+++ make_runit.py
@@ -10,14 +10,19 @@ import sys
 runit_file = "runit_template"
 task_filename = ["tasks", ".txt"]
 
-def make_runit(dest, time, nodes, cores, taskfile, tag=''):
+def make_runit(dest, time, nodes, cores, 
+               constraint, taskfile, tag=''):
     """ Makes a runit file from a template """
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    runit_loc = os.path.join(dir_path, runit_file)
+    
     # Load template file
-    with open(runit_file, 'r') as file:
+    with open(runit_loc, 'r') as file:
         runit_text = file.read()
     
     # Replace template info with user inputs
-    runit_text = runit_text.format(dest, time, nodes, cores, dest, taskfile)
+    runit_text = runit_text.format(dest, time, nodes, cores, 
+                                   constraint, dest, taskfile)
     
     # Save resulting runit file
     filename = os.path.join(dest, 'runit'+tag)
@@ -33,13 +38,14 @@ if __name__=='__main__':
     parser.add_argument("-t", "--time", type=str, default = '04:00:00', help = "Time for job to run")
     parser.add_argument("-N", "--nodes", type=str, default='15', help = "Number of nodes (>=2)")
     parser.add_argument("-c", "--cores", type=str, default='32', help = "Number of cores")
+    parser.add_argument("-C", "--constraint", type=str, default='cpu', help = "Arch constraint")
     parser.add_argument("-b", "--batch", nargs='?', type=int, help = "Number of batches")
     parser.add_argument('--tag', nargs='?', type=str, default='', help='Tag for runit file')
     args = parser.parse_args()
     
     # Check arguments
     if args.dest is None:
-        args.dest = os.path.dirname(args.taskfile)
+        args.dest = os.getcwd()
     
     # Make several runits
     if args.batch:
@@ -47,7 +53,8 @@ if __name__=='__main__':
         for i in range(args.batch):
             task_filename[-2] = f'_{i}' + ("_" if args.tag else "")
             taskfile = ''.join(task_filename)
-            make_runit(args.dest, args.time, args.nodes, args.cores, 
+            make_runit(args.dest, args.time, args.nodes, args.cores, args.constraint, 
                        ''.join(task_filename), tag=str(i))
     else: # Make one runit
-        make_runit(args.dest, args.time, args.nodes, args.cores, args.taskfile, tag=args.tag)
+        make_runit(args.dest, args.time, args.nodes, args.cores, args.constraint, 
+                   args.taskfile, tag=args.tag)
--- prep_run.py
+++ prep_run.py
@@ -8,13 +8,15 @@ import numpy as np
 import argparse
 import os
 import sys
+import util
+import sqlalchemy
 
 ### String to repeat in taskfarmer file
-shft_cmd = "shifter --image=eramey16/gradient:latest" \
+shft_cmd = "shifter --image=eramey16/monocle:latest" \
 " --volume='{}:/gradient_boosted/exports'" \
-" /opt/conda/bin/python /gradient_boosted/classify.py -r {} -d {} -rd {}"
+" /opt/conda/bin/python /gradient_boosted/classify.py --ls_id={}"
 dest_filename = ["tasks", ".txt"]
-coord_filename = ["coords", ".csv"]
+coord_filename = ["coords", "csv"]
 
 def process_tsv(file, dest):
     """ Processes a tsv file from the masterlens database """
@@ -31,49 +33,100 @@ def process_tsv(file, dest):
     # Return dataframe with coordinates
     return radec
 
-def fill_tasks(file, dest, rd):
+def fill_tasks(gal_data, dest, tag=''):
     """ Fills in shifter tasks with the ra and dec info in file """
     # Read in galaxy file
-    if os.path.exists(file):
-        galaxies = pd.read_csv(file)
+    if isinstance(gal_data, str):
+        if os.path.exists(file):
+            galaxies = pd.read_csv(file)
+        else:
+            print(f"No galaxy file found matching {gal_data}")
+            sys.exit(1)
     else:
-        print("No galaxy file found")
-        sys.exit(1)
+        galaxies = gal_data
     
     # Format columns
     galaxies.columns = [c.lower() for c in galaxies.columns]
     
-    if 'ra' not in galaxies.columns or 'dec' not in galaxies.columns:
-        print("Galaxy file does not contain ra, dec coordinate columns")
+    if 'ls_id' not in galaxies.columns:
+        print("Galaxy file does not contain ls_id column")
         sys.exit(1)
     
+    line = shft_cmd
+    if tag: line += f' --tag={tag}'
+    
     # Format ra and dec into shifter commands
-    galaxies['run'] = [shft_cmd.format(dest,a,b, rd) for a,b in zip(galaxies.ra, galaxies.dec)]
+    tasks = [line.format(dest,ls_id) for ls_id in galaxies.ls_id]
     
     # Save file commands
-    return galaxies['run']
+    return tasks
+
+def fill_from_db(dest, tag=''):
+    engine = sqlalchemy.create_engine(util.conn_string)
+    conn = engine.connect()
+    
+    query = f"SELECT * FROM bookkeeping WHERE stage=1"
+    if tag: query+=f" AND tag={tag}"
+    
+    bkdata = pd.DataFrame(conn.execute(query))
+    
+    tasks = fill_tasks(bkdata, dest, tag=tag)
+    
+    conn.close()
+    
+    return tasks
+
+def save_batches(tasks, n_batches, data_dir='.', dest_filename=dest_filename):
+    # if isinstance(tasks, list):
+    #     tasks = pd.Series(tasks)
+    if isinstance(dest_filename, str): dest_filename=dest_filename.split('.')
+    dest_filename.insert(-1, '_0')
+    
+    N = len(tasks)
+    start = 0
+    # Loop through batches
+    for i in range(n_batches):
+        # Get limits
+        end = int((i+1) / n_batches * N)
+
+        # Get dest filename
+        dest_filename[-2] = f'_{i}'
+        dest = os.path.join(data_dir, ''.join(dest_filename))
+
+        # Save batch to file
+        with open(dest, 'w+') as file:
+            file.write('\n'.join(tasks[start:end]))
+        # tasks.iloc[start:end].to_csv(dest, sep='\n', header=False, index=False, quoting=3)
+        print(f"File created: {dest}")
+
+        # Reset start
+        start = end
     
 if __name__=='__main__':
     # Parse command line arguments
     parser = argparse.ArgumentParser(description="Create a task file from a table of galaxy coordinates")
     parser.add_argument("-m", "--masterlens", action='store_true', 
                         help = "Indicates original file is a TSV from the masterlens database.")
-    parser.add_argument("-f", "--file", type=str, help = "Source file for target galaxies (must include RA and DEC).")
-    parser.add_argument("-d", "--dest", nargs='?', type=str, help = "Destination folder for generated lens files.")
-    parser.add_argument("-t", "--tag", nargs='?', type=str, default = '', help = "Tag for destination files.")
-    parser.add_argument("-rd", "--radius", nargs='?', type=str, default='.000277778', help="Search radius (in degrees).")
+    parser.add_argument("-f", "--file", type=str, default = None,
+                        help = "Source file for target galaxies (must include RA and DEC).")
+    parser.add_argument("-d", "--dest", nargs='?', type=str, 
+                        help = "Destination folder for generated lens files.")
+    parser.add_argument("-t", "--tag", nargs='?', type=str, default = '', 
+                        help = "Tag for database lookup.")
+    parser.add_argument("-rd", "--radius", nargs='?', type=str, default='.000277778', 
+                        help="Search radius (in degrees).")
     parser.add_argument("-b", "--batch", nargs='?', type=int, help = "Number of batches")
     args = parser.parse_args()
     
     # Check arguments
     if args.dest is None:
-        args.dest = os.path.dirname(args.file)
-    if args.tag:
-        dest_filename.insert(1, "_"+args.tag)
-        coord_filename.insert(1, "_"+args.tag)
+        if args.file is not None:
+            args.dest = os.path.dirname(args.file)
+        else:
+            args.dest = os.getcwd()
     
     # Check source file and destination folder
-    if not os.path.isfile(args.file):
+    if args.file is not None and not os.path.isfile(args.file):
         print(f"Source file does not exist: {args.file}")
         sys.exit(1)
     if not os.path.isdir(args.dest):
@@ -85,39 +138,25 @@ if __name__=='__main__':
         # Get csv with just RA, DEC
         radec = process_tsv(args.file, args.dest)
         # Save file
-        filename = ''.join(coord_filename)
+        filename = '.'.join(coord_filename)
         filename = os.path.join(args.dest, filename)
         radec.to_csv(filename, index=False)
         print(f"File created: {filename}")
         # Use new file for filling tasks
         args.file = filename
     
-    # Fill tasks from ra, dec in file
-    tasks = fill_tasks(args.file, args.dest, args.radius)
+    if args.file is not None: # Fill tasks from data in file
+        tasks = fill_tasks(args.file, args.dest, args.tag)
+    else: # Fill tasks from data in db
+        tasks = fill_from_db(args.dest, args.tag)
+    
     
     # Save in batches
     if args.batch:
-        dest_filename.insert(-1, '_0')
-        N = len(tasks)
-        start = 0
-        # Loop through batches
-        for i in range(args.batch):
-            # Get limits
-            end = int((i+1) / args.batch * N)
-            
-            # Get dest filename
-            dest_filename[-2] = f'_{i}'
-            dest = os.path.join(args.dest, ''.join(dest_filename))
-            
-            # Save batch to file
-            tasks.iloc[start:end].to_csv(dest, sep='\t', header=False, index=False, quoting=3)
-            print(f"File created: {dest}")
-            
-            # Reset start
-            start = end
+        save_batches(tasks, args.batch, data_dir=args.dest)
     else:
         # Save to destination (tasks.txt)
-        filename = ''.join(dest_filename)
+        filename = '.'.join(dest_filename)
         dest = os.path.join(args.dest, filename)
         tasks.to_csv(dest, sep='\t', header=False, index=False, quoting=3)
     
